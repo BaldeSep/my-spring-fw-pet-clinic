0691c54f7907c0fc38fa33aaca18e635a9032497
Baldemar Sepulveda <balde.sepu@pm.me>
2020-08-20 19:51:31 -0400

    Init Commit

---
+++ src/main/java/com/balde/petclinic/MySpringFwPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.balde.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringFwPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringFwPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/balde/petclinic/MySpringFwPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balde.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringFwPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
